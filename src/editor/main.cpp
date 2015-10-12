c90f1aa5e8073b475fe29cd46b53ff8ee74c5faf
FlorentUguet <florent.uguet@gmail.com>
2015-10-13 00:57:22 +0200

    Now using directly the MainWindow

--- src/editor/main.cpp
+++ src/editor/main.cpp
@@ -8,11 +8,11 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    //MainWindow w;
-    //w.show();
+    MainWindow w;
+    w.show();
 
-    EnnemyEditor e;
-    e.show();
+    //EnnemyEditor e;
+    //e.show();
 
     //QPathCreator *pathCreator = new QPathCreator(0);
     //pathCreator->show();
